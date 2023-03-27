526c1fbc709e8d97e7d64f6310af083dabfdecef
GuTessitore <gugatessi@gmail.com>
2023-03-27 16:16:31 -0300

    Update API extractor

--- src/extractor.py
+++ src/extractor.py
@@ -1,54 +1,78 @@
 import pandas as pd
 import requests
+import time
 import json
-import os
 
-url = "https://meteostat.p.rapidapi.com/stations/daily"
 
-querystring = {"station": "SBPR0", "start": "2021-01-02", "end": "2023-03-20"}
+class Extractor:
+    def __init__(self, lat, lon, start, end, api_key, units='metric'):
+        self.api_calls = 0
+        self.max_calls = 900
+        self.calls_per_minute = 50
+        self.dates_to_collect = pd.date_range(start, end, freq='D').astype(int) // 10 ** 9
+        self.api_key = api_key
+        self.lat = lat
+        self.lon = lon
+        self.units = units
+        self.start_time = None
+        self.data = None
 
-headers = {
-    "X-RapidAPI-Key": os.environ['RAPID_API_KEY'],
-    "X-RapidAPI-Host": "meteostat.p.rapidapi.com"
-}
+    def url(self, date):
+        return f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={self.lat}&lon={self.lon}&dt={date}&appid={self.api_key}&units={self.units}"
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-print(response)
+    def start_extraction(self):
+        self.start_time = time.time()
+        return self.extract()
 
-df = pd.DataFrame(json.loads(response.text)['data'])
+    def extract(self):
+        data = []
+        for date in self.dates_to_collect:
+            try:
+                mins = (time.time() - self.start_time) / 60 + 1
+                current_cpm = self.api_calls / mins
+                print(f"Current CPM: {current_cpm}")
+                print(f"Current API calls: {self.api_calls} in {mins:.2f} minutes")
+                if current_cpm > self.calls_per_minute:
+                    time_to_wait = 60 - (time.time() - self.start_time) % 60
+                    print(f"Waiting {time_to_wait} seconds")
+                    time.sleep(time_to_wait)
 
-# Code	Meaning
-# TAVG	Average Temperature
-# TMIN	Minimum Temperature
-# TMAX	Maximum Temperature
-# DWPT	Dew Point
-# PRCP	Total Precipitation
-# WDIR	Wind (From) Direction
-# WSPD	Average Wind Speed
-# WPGT	Wind Peak Gust
-# RHUM	Relative Humidity
-# PRES	Sea-Level Air Pressure
-# SNOW	Snow Depth
-# TSUN	Total Sunshine Duration
-# COCO	Weather Condition Code
+                if self.api_calls > self.max_calls:
+                    print("API calls exceeded")
+                    print(f"Last date call {date}")
+                    break
 
-rename_map = {
-    'date': 'Date',
-    'tavg': 'Average Temperature',  # Celsius
-    'tmin': 'Minimum Temperature',  # Celsius
-    'tmax': 'Maximum Temperature',  # Celsius
-    'prcp': 'Total Precipitation',  # Millimeters
-    'wdire': 'Wind (From) Direction',  # Degrees
-    'wspd': 'Average Wind Speed',  # Kilometers per hour
-    'pres': 'Sea-Level Air Pressure',  # hPa
-    'rhum': 'Relative Humidity',  # Percent
-    # 'snow': 'Snow Depth',  # Not used
-    # 'wpgt': 'Wind Peak Gust',  # Not used
-    # 'tsun': 'Total Sunshine Duration'  # Not used
-}
+                response = requests.get(self.url(date))
 
-df.drop(['snow', 'wpgt', 'tsun'], axis=1, inplace=True)
-df.rename(columns=rename_map, inplace=True)
+                if response.status_code == 200:
+                    data.append(response.json())
+                    self.api_calls += 1
+                else:
+                    print(f"Error: {response.status_code}")
+                    break
+            except Exception as e:
+                print(e)
+                break
 
-df.to_csv('../data/data.csv', index=False)
+        self.data = data
 
+    @property
+    def to_df(self):
+        data_to_df = [response["data"][0] for response in self.data]
+        df = pd.DataFrame(data_to_df)
+        return df
+
+
+start = "2021-01-01"
+end = "2023-03-01"
+
+api_key = "YOUR_API_KEY"
+lat = -23.555771
+lon = -46.639557
+
+extractor = Extractor(lat, lon, start, end, api_key)
+extractor.start_extraction()
+df = extractor.to_df
+
+filename = f"{start}_{end}.csv"
+df.to_csv(f"../data/{filename}", index=False)
