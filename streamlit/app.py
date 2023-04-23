39a2dced79f00638d48e4e7eb4c29d3ba13cb997
Isaac Higuchi <isaac_higuchi@outlook.com>
2023-04-23 16:57:15 -0300

    add image and app_traffic

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from datetime import datetime, timedelta
 import pandas as pd
-from functions_models import xgboost_regressor
 import folium
 from streamlit_folium import st_folium
 from geopy.geocoders import Nominatim
@@ -32,36 +31,7 @@ with col_main1:
     with col_mainsub2:
         st.metric(label='Estimated saving', value=f'💲 R$ {11_965.44:,.2f}')
 
-    #### TO BE REPLACED ######
-    # CSV's
-    df_irrad = pd.read_csv('data/openw_irradiation_2005_2020.csv')
-    df_irrad.drop(['day', 'weather_id'], axis=1, inplace=True)
-
-    y_pred = xgboost_regressor(df_irrad)  # irradiation predictions
-
-    # y_pred = [118.130264, 114.73563 , 101.42404 , 176.47775 , 112.80841 ,
-    #        157.99327 , 110.982155, 108.196976, 110.56889 , 137.60085 ,
-    #        110.87699 , 109.21667 , 102.76564 , 104.48606 , 167.93588 ,
-    #        109.639755, 188.73145 , 113.77642 , 139.08853 , 114.32364 ,
-    #        150.87944 , 150.07391 , 167.99814 , 133.70808 , 118.03198 ,
-    #        133.8792  , 142.81291 , 151.81943 , 145.09813 , 150.08461 ,
-    #        108.7888  , 154.68489 , 131.83202 , 119.56206 , 156.33952 ,
-    #        113.300896, 167.98074 , 153.09451 , 132.89561 , 149.40417 ,
-    #        212.66118 , 115.48822 , 165.70786 , 158.96251 , 104.79509 ,
-    #        167.0878  ,  96.5487  , 133.39662]
-
-    now = datetime.now()
-    current_month = now.month
-    current_year = now.year
-
-    dates = [datetime(current_year + ((current_month + i - 1) // 12), (current_month + i) % 12 or 12, 1) for i in
-             range(1, 33)]
-    df = pd.DataFrame({'data': dates,
-                       'irrad kwh/m2': y_pred})
-
-    ##########################################
-
-    st.line_chart(data=df, x='data', y='irrad kwh/m2')
+    # st.line_chart(data=df, x='data', y='irrad kwh/m2')
 
 with col_main2:
     # user inputs
@@ -104,8 +74,8 @@ with col_main2:
 
 
 #######################App###################################
-st.dataframe(df)
+# st.dataframe(df)
 
-# calc_energy
-calc_energy = df['irrad kwh/m2'].sum() * area * rend / 100
-st.write(f'A energia gerada para os próximos {range_data} anos é', round(calc_energy, 2), 'kwh')
+# # calc_energy
+# calc_energy = df['irrad kwh/m2'].sum() * area * rend / 100
+# st.write(f'A energia gerada para os próximos {range_data} anos é', round(calc_energy, 2), 'kwh')
